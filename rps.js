3d0f300084a34c06bf56b90c603b0f308f654880
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2023-06-12 04:56:59 -0700

    initial commit

---
+++ rps.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("button");
+
+const array = [Rock, Paper, Scissors];
+
+
+rock.addEventListener("click", (rockFunction = () => {
+    console.log("rock button clicked");
+    if(rock == scissors){
+        count += 1;
+    }
+    else{
+        count;
+        
+    }
+}));
+
+
+paper.addEventListener("click", (paperFunction = () => {console.log("paper button clicked");}));
+
+
+scissors.addEventListener("click", (scissorsFunction = () => {console.log("scissors button clicked");}));
